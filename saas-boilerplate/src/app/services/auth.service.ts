0bac57e9de057979bb0df1a0b2b3496df3b77a31
alvisf <alvisf0731@gmail.com>
2020-09-25 17:38:33 +0530

    fix again

--- saas-boilerplate/src/app/services/auth.service.ts
+++ saas-boilerplate/src/app/services/auth.service.ts
@@ -34,7 +34,7 @@ export class AuthService {
 
   async googleSignin() {
     const provider = new auth.GoogleAuthProvider();
-    const credential = await this.afAuth.signInWithRedirect(provider);
+    const credential = await this.afAuth.signInWithPopup(provider);
     return this.updateUserData(credential.user);
   }
 
